93d7e0c85f4878e802e47351be382ba2c2a08f66
jojoldu <jojoldu@gmail.com>
2015-11-21 00:46:02 +0900

    컨트롤러 사용 예제

--- public/js/todoApp.js
+++ public/js/todoApp.js
@@ -15,4 +15,14 @@ var model = {
 var todoApp = angular.module('todoApp', []);
 todoApp.controller("TodoController", function($scope){
     $scope.todo = model;
+
+    $scope.incompleteCount = function(){
+        var count = 0;
+        angular.forEach($scope.todo.items, function(item){
+           if(!item.done){
+               count++;
+           }
+        });
+        return count;
+    }
 });
