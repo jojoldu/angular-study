aecc15017e4bca648407d51d2ea3d3700433e1fa
unknown <>
2015-11-29 14:14:47 +0900

    ng-route 추가

--- public/js/controllers/product.js
+++ public/js/controllers/product.js
@@ -2,7 +2,20 @@
  * Created by user on 2015-11-22.
  */
 
-angular.module('product',['customFilter', 'cart'])
+angular.module('product',['customFilter', 'cart', 'ngRoute'])
+    .config(function($routeProvider){
+        $routeProvider.when('checkout', {
+            templateUrl : '/html/cart/checkoutSummary.html'
+        });
+
+        $routeProvider.when('products', {
+            templateUrl : '/html/productList.html'
+        });
+
+        $routeProvider.otherwise({
+            templateUrl: '/html/productList.html'
+        });
+    })
     .constant('url', 'http://localhost:8080/product')
     .controller('productCtrl', function($scope, $http, url){
        $scope.data={};
