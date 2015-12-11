c544b17e7aefa0417ec58d4d9c2a3c78960ca5b3
jojoldu <jojoldu@gmail.com>
2015-12-12 00:46:30 +0900

    디렉티브 예제

--- public/js/hello.js
+++ public/js/hello.js
@@ -11,4 +11,22 @@ angular.module('hello', [])
         $scope.toUpper = function(){
             $scope.hello.msg = $filter('uppercase')($scope.hello.msg);
         }
+
+        $scope.items = [
+            {
+                title : '볼펜',
+                count : 4,
+                price : 1800
+            },
+            {
+                title : '지우개',
+                count : 1,
+                price : 800
+            },
+            {
+                title : '연필',
+                count : 12,
+                price : 400
+            }
+        ];
     });
