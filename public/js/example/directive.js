5cf6666f67a05d07f65a3d10dfddb0e815bd7e3b
jojoldu <jojoldu@gmail.com>
2016-01-30 02:07:48 +0900

    <hello-world> 완성

--- public/js/example/directive.js
+++ public/js/example/directive.js
@@ -3,10 +3,16 @@
  */
 
 angular.module('example')
-.directive('ngInitFocus', function(){
-   return {
-       link : function(scope, element){
-           element[0].focus();
-       }
-   }
-});
+    .directive('ngInitFocus', function(){
+        return {
+            link : function(scope, element){
+                element[0].focus();
+            }
+        }
+    })
+    .directive('helloWorld', function(){
+        return {
+            restrict : 'E', // E:element, A:attribute, C:class, M:comment
+            template : '<div><h3>헬로우 월드</h3></div>'
+        }
+    });
