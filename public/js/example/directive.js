46140b3697bca04ce6a8663f20423b6dbbeb5c36
jojoldu <jojoldu@gmail.com>
2016-01-30 02:54:04 +0900

    <restricted> 완성 (restrict : A사용예제)

--- public/js/example/directive.js
+++ public/js/example/directive.js
@@ -19,11 +19,28 @@ angular.module('example')
     .directive('movie', function(){
         return {
             restrict:'E',
+            /*
+             scope : 디렉티브가 적용된 뷰의 scope,
+             element : 디렉티브가 적용된 html 엘리먼트,
+             attrs : 디렉티브가 적용된 html 엘리먼트의 어트리뷰트
+             */
             link: function(scope, element, attrs){
                 scope.username = attrs.username;
                 scope.reputation = attrs.reputation;
                 scope.img = attrs.img;
             },
             template:'<div><p><h3>영화소개</h3></p><p>Username: {{username}}</p> <p>Reputation: {{reputation}}</p> <img src="{{img}}" width="200" height="200"> </div>'
+        }
+    })
+    .directive('restricted', function(){
+        return {
+            restrict:'A',
+            link: function(scope, element, attrs){
+                var isAuth = Math.floor((Math.random() * 10) + 1) > 5;
+
+                if(!isAuth){
+                    element.css('display', 'none');
+                }
+            }
         }
     });
