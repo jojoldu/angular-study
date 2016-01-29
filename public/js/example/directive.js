99d1171957962b699e5234b9bab23664c563ce1b
jojoldu <jojoldu@gmail.com>
2016-01-30 02:24:41 +0900

    <movie> 완성 (restrice, link, template 동시 사용 예제)

--- public/js/example/directive.js
+++ public/js/example/directive.js
@@ -15,4 +15,15 @@ angular.module('example')
             restrict : 'E', // E:element, A:attribute, C:class, M:comment
             template : '<div><h3>헬로우 월드</h3></div>'
         }
+    })
+    .directive('movie', function(){
+        return {
+            restrict:'E',
+            link: function(scope, element, attrs){
+                scope.username = attrs.username;
+                scope.reputation = attrs.reputation;
+                scope.img = attrs.img;
+            },
+            template:'<div><p><h3>영화소개</h3></p><p>Username: {{username}}</p> <p>Reputation: {{reputation}}</p> <img src="{{img}}" width="200" height="200"> </div>'
+        }
     });
