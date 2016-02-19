0d0d99f13849c712b4e9c20d8c4b10a5f71ed0fa
jojoldu <jojoldu@gmail.com>
2016-02-20 00:19:33 +0900

    $routeParam으로 수정하기 기능 완성

--- public/js/example/controller.js
+++ public/js/example/controller.js
@@ -9,9 +9,15 @@ angular.module('example', ['ngRoute'])
             .when('/prev', {templateUrl:'html/example/prev.html'})
             .when('/products', {templateUrl:'html/example/products.html'})
             .when('/save', {templateUrl:'html/example/productForm.html'})
+            .when('/edit/:id', {templateUrl:'html/example/productForm.html'})
             .otherwise({redirectTo:'/home'});
     })
-    .controller('ExampleController', function($scope, $location){
+    /*
+        $route는 현재 선택된 route를 관리하는데 사용한다.
+        $routeChangeSuccess : route가 변경된 후 일어난다.
+
+     */
+    .controller('ExampleController', function($scope, $location, $route, $routeParams){
         $scope.message = {
             text : '아무것도 클릭되지 않음'
         };
@@ -39,9 +45,39 @@ angular.module('example', ['ngRoute'])
         $scope.products = products;
 
         $scope.saveProduct = function(product){
-            product.id=$scope.products.length;
-            $scope.products.push(product);
+            var isNew = true;
+            for(var i=0;i<$scope.products.length;i++){
+                if($scope.products[i].id == product.id){
+                    $scope.products[i] = product;
+                    $scope.product={};
+                    isNew = false;
+                    break;
+                }
+            }
+            if(isNew){
+                product.id=$scope.products.length+1;
+                $scope.products.push(product);
+            }
             $location.path('/products');
-        }
+        };
+
+        /*
+            $route는 사용할곳이 많지 않다.
+            크게 2가지인데, 1) route가 변하는 시점 2) 새 경로
+            $routeChangeSuccess는 1)을 알려주고,
+            $location은 2)을 알려준다.
+
+         */
+        $scope.$on('$routeChangeSuccess', function(){
+            if($location.path().indexOf('/edit/') == 0){ //새로운 route경로가 /edit/로 시작한다면 아래 코드를 시작
+                var id = $routeParams['id'];
+                for(var i=0;i<$scope.products.length;i++){
+                    if($scope.products[i].id == id){
+                        $scope.product = $scope.products[i];
+                        break;
+                    }
+                }
+            }
+        })
 
     });
