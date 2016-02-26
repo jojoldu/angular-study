40f37ca2c87813e183a64bdde2f8236c114f07b2
jojoldu <jojoldu@gmail.com>
2016-02-26 20:30:44 +0900

    8080 포트로 변경

--- app.js
+++ app.js
@@ -149,5 +149,5 @@ app.get('/example', function(req, res){
     res.sendFile(path.join(__dirname + '/public/html/example.html'));
 });
 
-app.listen(80);
-console.log('Express Listening on port 80...');
+app.listen(8080);
+console.log('Express Listening on port 8080...');
